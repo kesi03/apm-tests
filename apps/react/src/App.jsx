daf2020361c32239f21d3df3ad160b3ebc561ee2
kester simm <kesi03@icloud.com>
2026-09-03 10:30:07 +0100

    Updated version

--- apps/react/src/App.jsx
+++ apps/react/src/App.jsx
@@ -179,7 +179,7 @@ export default function App() {
     <ErrorBoundary>
       <main>
         <h1>React + Elastic APM RUM</h1>
-        <h2>Version 1.0.0</h2>
+        <h2>Version 1.0.2</h2>
         <p>This page is instrumented with the Elastic APM RUM agent.</p>
         <StartChain />
         <SendTransaction />
